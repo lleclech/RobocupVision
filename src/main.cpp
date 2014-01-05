c6ae83e220182a618ac4f4705c9e850c957bbdd6
scauglog <scauglog@gmail.com>
2014-01-05 17:51:49 +0100

    maha calc

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
-
+#include <cstring>
+#include <fstream>
 #include <opencv2/opencv.hpp>
 #include <unistd.h>
 #include <opencv2/core/core.hpp>
@@ -9,41 +10,63 @@
 
 using namespace std;
 using namespace cv;
+template<typename T>
+
+
+std::string to_string( const T & Value )
+{
+  // utiliser un flux de sortie pour créer la chaîne
+  std::ostringstream oss;
+  // écrire la valeur dans le flux
+  oss << Value;
+  // renvoyer une string
+  return oss.str();
+}
+
+bool terrain(double R, double G){
+    Mat mu(1,2,CV_64F),cov(2,2,CV_64F), point(1,2,CV_64F);
+    double seuil=2000;
+    //Mat::zeros(bbin*scale, abin*scale, CV_8UC3);
+    mu.at<double>(0,0)=187.26119;
+    mu.at<double>(0,1)=154.00789;
+    point.at<double>(0,0)=R;
+    point.at<double>(0,1)=G;
+    cov.at<double>(0,0)=406.11914;
+    cov.at<double>(0,1)=491.43532;
+    cov.at<double>(1,0)=491.43532;
+    cov.at<double>(1,1)=723.74627;
+    return Mahalanobis(point,mu,cov)<seuil;
+}
 
 int main(){
-  // Initialize capturing live feed from the camera
-  VideoCapture capture(0);
-  //printf("%f",capture.get(CV_CAP_PROP_FRAME_WIDTH));
-  //printf("%f",capture.get(CV_CAP_PROP_FRAME_HEIGHT));
-
-  // Couldn't get a device? Throw an error and quit
-  if(!capture.isOpened()){
-    printf("Could not initialize capturing...\n");
-    return -1;
-  }
+    String path = "../data/log2/";
+    String filename = "rgb";
+    String ext=".png";
+    int nbImg=150;
 
   // The two windows we'll be using
-  namedWindow("video",1);
-  namedWindow("modifVideo",1);
+  namedWindow("image",1);
+  namedWindow("modif image",1);
 
   Mat frame, frameModif;
-  while(true){
-    capture >> frame;
-    pyrDown(frame, frame, Size(frame.cols/2,frame.rows/2));
-    cvtColor(frame,frameModif,CV_BGR2GRAY);
-    
-    for(int i=0; i<frame.rows; i++){
-      for(int j=0; j<frame.cols; j++){
-    	frameModif.at<char>(i,j) = 255;
-      }
+  for(int nb=0;nb<nbImg;nb++){
+      frame = imread(path+filename+to_string(nb)+ext);
+      frameModif = frame;
+    imshow("image", frame);
+    for( int x = 0; x < frame.cols; x++ ){
+        for( int y = 0; y < frame.rows; y++ ){
+            Vec3b pixel=frame.at<Vec3b>(y,x);
+            if(terrain(pixel[0],pixel[1])){
+                frameModif.at<Vec3b>(y,x)=Vec3b(0,0,0);
+            }else{
+                frameModif.at<Vec3b>(y,x)=Vec3b(250,250,250);
+            }
+           }
     }
-    //inRange(frameModif,Scalar(0,20,20),Scalar(100,230,230), frameModif);
-    
-    imshow("video", frame);
-    imshow("modifVideo", frameModif);
+    imshow("modif image", frameModif);
 
     // Wait for a keypress
-    int c = waitKey(10);
+    int c = waitKey(1000);
     if(c!=-1){
       // If pressed, break out of the loop
       break;
