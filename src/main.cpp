c619a1e6d76b700ec79adefe00e536b06d29e03f
Christian LASSERRE <classerre@enseirb.fr>
2014-01-05 18:15:24 +0100

    MAHALABAR calcule à la main...

--- src/main.cpp
+++ src/main.cpp
@@ -24,21 +24,38 @@ std::string to_string( const T & Value )
 }
 
 bool terrain(double R, double G){
-    Mat mu(1,2,CV_64F),cov(2,2,CV_64F), point(1,2,CV_64F);
-    double seuil=2000;
-    //Mat::zeros(bbin*scale, abin*scale, CV_8UC3);
-    mu.at<double>(0,0)=187.26119;
-    mu.at<double>(0,1)=154.00789;
-    point.at<double>(0,0)=R;
-    point.at<double>(0,1)=G;
-    cov.at<double>(0,0)=406.11914;
-    cov.at<double>(0,1)=491.43532;
-    cov.at<double>(1,0)=491.43532;
-    cov.at<double>(1,1)=723.74627;
-    return Mahalanobis(point,mu,cov)<seuil;
+  /*Mat mu(1,2,CV_64F),cov(2,2,CV_64F), point(1,2,CV_64F);
+  double seuil=2000;
+  //Mat::zeros(bbin*scale, abin*scale, CV_8UC3);
+  mu.at<double>(0,0)=;
+  mu.at<double>(0,1)=;
+  point.at<double>(0,0)=R;
+  point.at<double>(0,1)=G;
+  cov.at<double>(0,0)=;
+  cov.at<double>(0,1)=;
+  cov.at<double>(1,0)=;
+  cov.at<double>(1,1)=;
+  return Mahalanobis(point,mu,cov)<seuil;
+  */
+  double seuil = 4.;
+  double e1 = 406.11914,
+    e2 = 491.43532,
+    e3 = 491.43532,
+    e4 = 723.74627;
+  double mu1 = 187.26119,
+    mu2 = 154.00789;
+
+  double a = R-mu1,
+    b = G - mu2;
+  double det = e1*e4-e2*e3;
+  double eps1 = e4/det,
+    eps2 = - e2/det,
+    eps3 = - e3/det,
+    eps4 = e1/det;
+  return  eps1*a*a + eps2*a*b + eps3*a*b + eps4*b*b < seuil;
 }
 
-int main(){
+int main(void){
     String path = "../data/log2/";
     String filename = "rgb";
     String ext=".png";
@@ -56,7 +73,7 @@ int main(){
     for( int x = 0; x < frame.cols; x++ ){
         for( int y = 0; y < frame.rows; y++ ){
             Vec3b pixel=frame.at<Vec3b>(y,x);
-            if(terrain(pixel[0],pixel[1])){
+            if(terrain(pixel[2],pixel[1])){
                 frameModif.at<Vec3b>(y,x)=Vec3b(0,0,0);
             }else{
                 frameModif.at<Vec3b>(y,x)=Vec3b(250,250,250);
