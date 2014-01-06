ab8abca33d77ce230d52d19f0043af0cb45f71b8
Christian LASSERRE <classerre@enseirb.fr>
2014-01-06 09:36:17 +0100

    maj

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,8 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
+#include "segmentation/Segmentation.cpp"
+ 
 using namespace std;
 using namespace cv;
 template<typename T>
@@ -23,65 +25,39 @@ std::string to_string( const T & Value )
   return oss.str();
 }
 
-bool terrain(double R, double G){
-  /*Mat mu(1,2,CV_64F),cov(2,2,CV_64F), point(1,2,CV_64F);
-  double seuil=2000;
-  //Mat::zeros(bbin*scale, abin*scale, CV_8UC3);
-  mu.at<double>(0,0)=;
-  mu.at<double>(0,1)=;
-  point.at<double>(0,0)=R;
-  point.at<double>(0,1)=G;
-  cov.at<double>(0,0)=;
-  cov.at<double>(0,1)=;
-  cov.at<double>(1,0)=;
-  cov.at<double>(1,1)=;
-  return Mahalanobis(point,mu,cov)<seuil;
-  */
-  double seuil = 10.000000;
-  double e1 = 61.276426,
-    e2 = 181.41617,
-    e3 = 181.41617,
-    e4 = 854.30509;
-  double mu1 = 226.10368,
-    mu2 = 184.65723;
-
-  double a = R-mu1,
-    b = G - mu2;
-  double det = e1*e4-e2*e3;
-  double eps1 = e4/det,
-    eps2 = - e2/det,
-    eps3 = - e3/det,
-    eps4 = e1/det;
-  return  eps1*a*a + eps2*a*b + eps3*a*b + eps4*b*b < seuil*seuil;
-}
-
 int main(void){
-    String path = "../data/log2/";
-    String filename = "rgb";
-    String ext=".png";
-    int nbImg=150;
+  String path = "../data/log2/";
+  String filename = "rgb";
+  String ext=".png";
+  int nbImg=150;
 
   // The two windows we'll be using
   namedWindow("image",1);
   namedWindow("modif image",1);
 
+  // The three object we're interesting in
+  Segmentation but = new But();
+  //Segmentation ball = new Ball();
+  //Segmentation ligne = new Ligne(); 
+  //Segmentation terrain = new Terrain();
+  
   Mat frame, frameModif;
   for(int nb=0;nb<nbImg;nb++){
-      frame = imread(path+filename+to_string(nb)+ext);
-      frameModif = frame;
+    frame = imread(path+filename+to_string(nb)+ext);
+    frameModif = frame;
     imshow("image", frame);
-    for( int x = 0; x < frame.cols; x++ ){
-        for( int y = 0; y < frame.rows; y++ ){
-            Vec3b pixel=frame.at<Vec3b>(y,x);
-            if(terrain(pixel[2],pixel[1])){
-                frameModif.at<Vec3b>(y,x)=Vec3b(0,0,0);
-            }else{
-                frameModif.at<Vec3b>(y,x)=Vec3b(250,250,250);
-            }
-           }
+    for(int x = 0; x < frame.cols; x++ ){
+      for( int y = 0; y < frame.rows; y++ ){
+	Vec3b pixel=frame.at<Vec3b>(y,x);
+	if(but.Mahalanobis(pixel[2],pixel[1])){
+	  frameModif.at<Vec3b>(y,x)=Vec3b(0,0,0);
+	}else{
+	  frameModif.at<Vec3b>(y,x)=Vec3b(250,250,250);
+	}
+      }
     }
     imshow("modif image", frameModif);
-
+      
     // Wait for a keypress
     int c = waitKey(1000);
     if(c!=-1){
